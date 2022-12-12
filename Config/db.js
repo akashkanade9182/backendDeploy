91bcbb2435e0ec81a5595edb6abae999599ed387
Akash Mahadeo Kanade <86785346+akashkanade9182@users.noreply.github.com>
2022-12-12 10:35:33 +0530

    Update db.js

--- Config/db.js
+++ Config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/authnxm")
+const connection = mongoose.connect("mongodb://127.0.0.1:27017/notedb")
 
 
 module.exports = {
