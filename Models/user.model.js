8a69338a4afb58be81f986fa97d917bd95fb725a
akashkanade9182 <akashkanade235@gmail.com>
2023-02-21 13:06:46 +0530

    blog project added

--- Models/user.model.js
+++ Models/user.model.js
@@ -2,11 +2,11 @@ const mongoose=require("mongoose")
 
 
 const userSchema=mongoose.Schema({
-   name:{type:String,require:true},
-   category: {type:String,require:true},
-   difficulty: {type:String,require:true},
-   quetion:{type:Number,require:true},
- 
+    name : {type:String,require:true},
+    avatar:{type:String,require:true},
+   email: {type:String,require:true},
+   password: {type:String,require:true},
+
     
 })
 
